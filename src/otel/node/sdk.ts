531a8326d59bdf6af2f5826ed8987c08f8b3aef2
YunosukeY <y_yamada@dreamarts.co.jp>
2024-02-13 20:23:38 +0900

    propagate

--- src/otel/node/sdk.ts
+++ src/otel/node/sdk.ts
@@ -3,10 +3,12 @@ import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
 import { Resource } from "@opentelemetry/resources";
 import { SemanticResourceAttributes } from "@opentelemetry/semantic-conventions";
 import { SimpleSpanProcessor } from "@opentelemetry/sdk-trace-node";
+import { B3Propagator } from "@opentelemetry/propagator-b3";
 
 export const nodeSdk = new NodeSDK({
   resource: new Resource({
     [SemanticResourceAttributes.SERVICE_NAME]: "server",
   }),
+  textMapPropagator: new B3Propagator(),
   spanProcessor: new SimpleSpanProcessor(new OTLPTraceExporter()),
 });
