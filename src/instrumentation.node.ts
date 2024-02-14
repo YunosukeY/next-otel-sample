8e6803fb2a3e3f1be5e6c6b5a5ef05f36a5dab67
YunosukeY <y_yamada@dreamarts.co.jp>
2024-02-14 20:23:56 +0900

    sdk

--- src/instrumentation.node.ts
+++ src/instrumentation.node.ts
@@ -1,3 +1,12 @@
 import { nodeSdk } from "./otel/node/sdk";
 
 nodeSdk.start();
+
+if (process.env.NEXT_MANUAL_SIG_HANDLE) {
+  process.on("SIGTERM", () => {
+    nodeSdk.shutdown();
+  });
+  process.on("SIGINT", () => {
+    nodeSdk.shutdown();
+  });
+}
--- src/otel/node/sdk.ts
+++ src/otel/node/sdk.ts
@@ -1,14 +1,29 @@
 import { NodeSDK } from "@opentelemetry/sdk-node";
 import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
-import { Resource } from "@opentelemetry/resources";
-import { SemanticResourceAttributes } from "@opentelemetry/semantic-conventions";
 import { BatchSpanProcessor } from "@opentelemetry/sdk-trace-node";
-import { W3CTraceContextPropagator } from "@opentelemetry/core";
+import { PeriodicExportingMetricReader } from "@opentelemetry/sdk-metrics";
+import { OTLPMetricExporter } from "@opentelemetry/exporter-metrics-otlp-http";
+import {
+  BatchLogRecordProcessor,
+  ConsoleLogRecordExporter,
+} from "@opentelemetry/sdk-logs";
 
 export const nodeSdk = new NodeSDK({
-  resource: new Resource({
-    [SemanticResourceAttributes.SERVICE_NAME]: "server",
-  }),
-  textMapPropagator: new W3CTraceContextPropagator(),
+  serviceName: "server",
+
+  // config for TraceProvider
   spanProcessor: new BatchSpanProcessor(new OTLPTraceExporter()),
+
+  // config for MeterProvider
+  metricReader: new PeriodicExportingMetricReader({
+    exporter: new OTLPMetricExporter({
+      url: "http://localhost:4318/v1/metrics",
+    }),
+    exportIntervalMillis: 1000,
+  }),
+
+  // config for LoggerProvider
+  logRecordProcessor: new BatchLogRecordProcessor(
+    new ConsoleLogRecordExporter()
+  ),
 });
