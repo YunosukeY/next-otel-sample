de59f335ed1e8262ac14040473974865f81acceb
YunosukeY <y_yamada@dreamarts.co.jp>
2024-02-15 16:25:37 +0900

    refactor

--- src/otel/web/webLogger.ts
+++ src/otel/web/webLogger.ts
@@ -1,14 +1,14 @@
 import { logs } from "@opentelemetry/api-logs";
 import {
+  BatchLogRecordProcessor,
   LoggerProvider,
-  SimpleLogRecordProcessor,
 } from "@opentelemetry/sdk-logs";
 import { webResource } from "./webResource";
 import { OTLPLogExporter } from "@opentelemetry/exporter-logs-otlp-http";
 
 const loggerProvider = new LoggerProvider({ resource: webResource });
 loggerProvider.addLogRecordProcessor(
-  new SimpleLogRecordProcessor(new OTLPLogExporter({ url: "/api/logs" }))
+  new BatchLogRecordProcessor(new OTLPLogExporter({ url: "/api/logs" }))
 );
 logs.setGlobalLoggerProvider(loggerProvider);
 
